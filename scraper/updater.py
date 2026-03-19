b6844bac9d04407ab09042532334793e9a87f582
Murtokov <MurtukovL@yandex.ru>
2026-03-19 23:26:50 +0300

    really?

--- scraper/updater.py
+++ scraper/updater.py
@@ -12,6 +12,7 @@ class OddsUpdater:
                 data = self.scrapper.find_match_odds()
 
                 for (p1, p2), (odds_a, odds_b) in data.items():
+                    print("SAVING TO REDIS:", p1, p2)
                     self.match_cache.save_match(p1, p2, odds_a, odds_b)
 
             except Exception as e:
